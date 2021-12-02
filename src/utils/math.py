26a9ea3e9858acb57b5d0b2d13a3124c337e2c05
yohann.va <yohann.valo@gmail>
2021-12-02 13:44:56 +0100

    expo

--- src/utils/math.py
+++ src/utils/math.py
@@ -1,2 +1,18 @@
-def exponentiation():
-  
+def exponentiation(a, n, m):
+
+    b = 1
+    c = a
+    i = 0
+    pow = 1
+
+    while n > 0:
+        print(f'[ {i} ] {a} ^ {pow:2d} mod {m} = {c % m}')
+        k = n % 2
+        n = n >> 1
+        if k == 1:
+            b = b * c % m
+        c = c * c % m
+        i += 1
+        pow *= 2
+
+    return b % m
