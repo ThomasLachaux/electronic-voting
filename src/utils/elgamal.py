5b7b352b577b5fb61ff14e6087c1bb699d3a18bd
yohann.va <yohann.valo@gmail>
2021-12-31 10:10:23 +0100

    el gamal works

--- src/utils/elgamal.py
+++ src/utils/elgamal.py
@@ -6,13 +6,14 @@ def encrypt(message, pubkey, p , g):
   c2 = utils.math.exponentiation(g, r, p)
   
   k = utils.math.exponentiation(pubkey, r, p)
-  c1 = message * k
+  c1 = message * k % p
 
   return c1, c2
 
 def decrypt(c1, c2, p, election_privkey):
   k = utils.math.exponentiation(c2, election_privkey, p)
-  k_1 =  utils.math.invert(k,p)
-  m = c1 * k_1
+  p_1_x = p - 1 - election_privkey
+  k_1 =  utils.math.exponentiation(c2, p_1_x, p)
+  m = c1 * k_1 % p
 
   return m
