9d637529880611dc0c83faa103c87e2157a23730
yohann.va <yohann.valo@gmail>
2022-01-03 11:29:01 +0100

    zero knowledge proof

--- src/utils/zero_knowledge_proof.py
+++ src/utils/zero_knowledge_proof.py
@@ -6,23 +6,29 @@ import utils.password
 def prove(g, p, election_id):
 
   w = random.randint(1, p)
-  a = utils.math.exponentiation(g, w, p)
-
+  a = utils.math.exponentiation(g, w, p) 
+  print(a)
+  print("\n")
   # TODO Passer su PyInquierer
   secret = input("enter your password: ")
+
   s = utils.password.generate_pkdf2(secret, election_id)
-  print(s)
-  chal_proof = hashlib.sha256(str(s + a).encode())
+  pubkey = utils.math.exponentiation(g, s, p)
+  chal_proof = hashlib.sha256(str(pubkey + a).encode())
   val = int.from_bytes(chal_proof.digest(), 'big')
-  resp = (w - val * int(secret, 36)) % p
+
+  secret_encoded = secret.encode()
+  secret_int = int.from_bytes(secret_encoded, 'big')
+  resp = (w - val * secret_int) % p 
 
   return resp, val
 
 def verify(g, p, resp, s, chal):
   a = (utils.math.exponentiation(g, resp, p) * utils.math.exponentiation(s, chal, p)) % p
-  print(s)
+  print(a)
   chal_verify = hashlib.sha256(str(s + a).encode())
   val = int.from_bytes(chal_verify.digest(), 'big')
+
   if val == chal:
     print("success")
   else:
