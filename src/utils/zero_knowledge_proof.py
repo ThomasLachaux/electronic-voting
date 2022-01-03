daa24e0f9873b100bfb85e76d31ace79482f0727
Thomas de Lachaux <thomas.delachaux@gmail.com>
2022-01-03 12:53:25 +0100

    Zero knowledge proff working !
    Co-authored-by: Yohann <yohannValo@users.noreply.github.com>

--- src/utils/zero_knowledge_proof.py
+++ src/utils/zero_knowledge_proof.py
@@ -7,29 +7,27 @@ def prove(g, p, election_id):
 
   w = random.randint(1, p)
   a = utils.math.exponentiation(g, w, p) 
-  print(a)
-  print("\n")
+
   # TODO Passer su PyInquierer
+  # Mot de passe utilisateur
   secret = input("enter your password: ")
 
+  # Dérivation de clé
   s = utils.password.generate_pkdf2(secret, election_id)
+
+  # Génération de clé publique
   pubkey = utils.math.exponentiation(g, s, p)
-  chal_proof = hashlib.sha256(str(pubkey + a).encode())
-  val = int.from_bytes(chal_proof.digest(), 'big')
 
-  secret_encoded = secret.encode()
-  secret_int = int.from_bytes(secret_encoded, 'big')
-  resp = (w - val * secret_int) % p 
+  # Génération du challenge
+  challenge_proof = hashlib.sha256(str(pubkey + a).encode())
+  challenge = int.from_bytes(challenge_proof.digest(), 'big')
 
-  return resp, val
+  challenge_response = (w - s * challenge) % p 
+  return challenge_response, challenge
 
-def verify(g, p, resp, s, chal):
-  a = (utils.math.exponentiation(g, resp, p) * utils.math.exponentiation(s, chal, p)) % p
-  print(a)
-  chal_verify = hashlib.sha256(str(s + a).encode())
-  val = int.from_bytes(chal_verify.digest(), 'big')
+def verify(g, p, challenge_response, s, challenge_proof):
+  a = (utils.math.exponentiation(g, challenge_response, p) * utils.math.exponentiation(s, challenge_proof, p)) % p
+  challenge_verify = hashlib.sha256(str(s + a).encode())
+  challenge = int.from_bytes(challenge_verify.digest(), 'big')
 
-  if val == chal:
-    print("success")
-  else:
-    print("c'est pas bon")
+  return challenge == challenge_proof
