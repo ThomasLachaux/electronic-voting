6c831e399c7df4c20cde6c1e25798b2d940780d0
Thomas de Lachaux <thomas.delachaux@gmail.com>
2022-01-05 12:04:36 +0100

    Elgamal signing working
    Co-authored-by: Yohann <yohannValo@users.noreply.github.com>

--- src/utils/elgamal.py
+++ src/utils/elgamal.py
@@ -1,6 +1,10 @@
 import random
 import utils.math
 from utils.constants import p, g
+import hashlib
+import utils.prime
+import utils.math
+import math
 
 def encrypt(message, pubkey):
   r = random.randint(1, p)
@@ -12,9 +16,37 @@ def encrypt(message, pubkey):
   return c1, c2
 
 def decrypt(c1, c2, election_privkey):
-  # TODO: c'est quoi ce k ?
   p_1_x = p - 1 - election_privkey
   k_1 =  utils.math.exponentiation(c2, p_1_x, p)
   m = c1 * k_1 % p
 
   return m
+
+def sign (privkey_signer, message):
+  s = 0
+  while s == 0:
+
+    # Generate a prime number relative to p - 1
+    while True:
+      k = random.randint(2, p - 2)
+
+      if math.gcd(k, p - 1) == 1:
+        break
+  
+    r = utils.math.exponentiation(g, k, p)
+    k_1 = pow(k, -1, p - 1)
+
+    h_object = hashlib.sha256(str(message).encode())
+    h = int.from_bytes(h_object.digest(), 'big')
+    s = ((h - privkey_signer * r ) * k_1) % (p - 1)
+
+  return r, s
+
+def verify_signature(message, publik_key, r, s):
+  
+  h_object = hashlib.sha256(str(message).encode())
+  h = int.from_bytes(h_object.digest(), 'big')
+  signature = (utils.math.exponentiation(publik_key, r, p) * utils.math.exponentiation(r, s, p)) % p
+
+  return signature == utils.math.exponentiation(g, h, p)    
+
