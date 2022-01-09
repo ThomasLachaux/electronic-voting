9725274c482ebeddb558eef5bc55deec95411961
Thomas de Lachaux <thomas.delachaux@gmail.com>
2022-01-09 11:19:16 +0100

    Clear all data

---
+++ README.md
@@ -0,0 +1,13 @@
+# Electronic voting
+
+## Installation
+
+Ce programme fonctionne sur Python 3.8. Vous aurez besoin de pipenv, installable avec `pip3 install pipenv`.
+
+Installez ensuite les dépendances du projet avec `pipenv install`. Entrez dans le venv avec `pipenv shell`.
+
+Vous pouvez désormais lancer le projet avec la commande `python3 src/main.py`.
+
+Pour lancer un vote, la première étape sera de créer des electeurs et ensuite de créer une éléction.
+
+A noter que les clés privées des utilisateurs sont actuellement stockées dans la base de donnée par soucis de confort. Le code ne les utilise néanmoins en aucun cas.
--- src/menu/check_vote.py
+++ src/menu/check_vote.py
@@ -1,10 +1,21 @@
 from PyInquirer.prompt import prompt
+from rich import box
+from rich.align import Align
 
 from utils.terminal import print_title
+from rich.console import Console
+from rich.table import Table
+import shutil
 
 
 def entrypoint(a, e, s):
   print_title('Vérifier un vote')
+  console = Console()
+
+  term_columns, term_rows = shutil.get_terminal_size()
+  table = Table(expand=True, box=box.SQUARE, width=int(term_columns * 3 / 4))
+
+  table.add_column('Signatures', style='green', justify='center')
 
   questions = [
     {
@@ -17,9 +28,14 @@ def entrypoint(a, e, s):
 
   answer = prompt(questions)
   election_id = answer['election']
-  
-  print('Voici la liste des signatures pour cette election')
-  for index, signature in enumerate(s.elections[election_id]['signatures']):
-    print(f'{index + 1} - {signature}')
 
+  console.clear()
+  print_title('Vérifier un vote')
+  
+  for index, signature in enumerate(s.elections[election_id]['signatures']):
+    table.add_row(signature)
+
+  table = Align.center(table)
+  console.print(table)
+  input()
   
